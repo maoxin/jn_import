ff94a74b757bb24ac562e878ec3cfb7ee1f8e5c0
maoxin <maoxin.horizon@gmail.com>
2016-09-07 17:49:20 +0800

    fix some bugs

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='jn_import',
-      version='0.3',
+      version='0.4',
       description='Jupyter Notebooks Importing Tools',
       url='https://github.com/maoxin/jn_import',
-      download_url='https://github.com/maoxin/jn_import/tarball/0.3',
+      download_url='https://github.com/maoxin/jn_import/tarball/0.4',
       author='maoxin',
       author_email='maoxin.horizon@gmail.com',
       license='MIT',
