4d0ef82a51cf88bfde759f301f720083d7de55ba
maoxin <maoxin.horizon@gmail.com>
2016-09-07 16:56:32 +0800

    V0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jn_import',
+      version='0.1',
+      description='Jupyter Notebooks Importing Tools',
+      url='https://github.com/maoxin/jn_import',
+      author='maoxin',
+      author_email='maoxin.horizon@gmail.com',
+      license='MIT',
+      packages=['jn_import'],
+      zip_safe=False)
